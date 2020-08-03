d70c221b7da933fefbd865065c1a7ef31059565d
XIRAN YIN <xirany2@illinois.edu>
2020-08-03 01:41:11 -0500

    comment

--- function.js
+++
@@ -1,8 +0,0 @@
-function getToolTipDiv() {
-    return d3.selectAll("div").filter("#tooltip");
-}
-
-function getToolTipText(datum, shouldshowCylinders) {
-    var cylinderText = datum.EngineCylinders == "0" ? "N/A" : datum.EngineCylinders;
-    return "Maker: " + datum.Make + "<br/># Cylinders: " + cylinderText + "<br/>Highway Mileage: " + datum.AverageHighwayMPG + "<br/>City Mileage: " + datum.AverageCityMPG;
-}
